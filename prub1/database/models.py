ae40f1ea058c9fd14c8e0a864d22e7b29bd75424
ositogominola <86389086+ositogominola@users.noreply.github.com>
2022-07-22 20:35:45 -0500

    Update models.py

--- prub1/database/models.py
+++ prub1/database/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class user(AbstractUser):
@@ -11,18 +10,18 @@ class user(AbstractUser):
 class producto(models.Model):
     ID_producto=models.AutoField(primary_key=True)
     nombre_producto=models.CharField(max_length=30)
-    descripcion=models.CharField(max_length=250)
+    descripcion=models.CharField(max_length=250,null=True,blank=True)
     category=models.CharField(max_length=30)
-    imagen=models.ImageField()
+    imagen=models.ImageField(upload_to='imageProd',null=True,blank=True)
     stock=models.IntegerField()
     precio_unitario=models.IntegerField()
-    calificacion=models.IntegerField()
-    descuento=models.IntegerField()
+    calificacion=models.IntegerField(null=True,blank=True)
+    descuento=models.IntegerField(null=True,blank=True)
 
 class factura(models.Model):
     ID_factura=models.AutoField(primary_key=True)
     fecha=models.DateField()
-    forma_pago=models.CharField(max_length=20)
+    forma_pago=models.CharField(max_length=20,null=True,blank=True)
     valor_compra=models.IntegerField()
     estado=models.CharField(max_length=20)
     Id_cliecte=models.ForeignKey(user,null=True,blank=True,on_delete=models.CASCADE)
@@ -31,4 +30,5 @@ class factura_producto(models.Model):
     ID_facPro=models.AutoField(primary_key=True)
     Id_producto=models.ForeignKey(producto,null=True,blank=True,on_delete=models.CASCADE)
     Id_factura=models.ForeignKey(factura,null=True,blank=True,on_delete=models.CASCADE)
-    cantidad=models.IntegerField(default=0)
+    cantidad=models.IntegerField(null=True,blank=True)
+   
