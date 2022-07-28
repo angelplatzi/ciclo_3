fe6b0ab22f8c5e8d6b95eadc63dddca4f543641d
CrisCasas <cristiancasas706@gmail.com>
2022-07-27 19:32:29 -0500

    sin runserver

--- prub/prub1/urls.py
+++ prub/prub1/urls.py
@@ -4,11 +4,16 @@ from django.urls import path, include
 from tienda import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.urls import urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", include('loginandregistrer.urls')),
-    path('tienda/',include('tienda.urls'))
+    path('tienda/',include('tienda.urls')),
+    path('', include('login.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
