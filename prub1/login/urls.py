0fa7b27e67e873a289116f5a70331c0bba98b35e
CrisCasas <108154186+CrisCasas@users.noreply.github.com>
2022-07-27 22:06:11 -0500

    Delete prub1 directory

--- prub1/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib.auth.urls import urlpatterns
-from login.views import logueando
-
-urlpatterns = [
-    path('', logueando, name="logueando")    
-]
